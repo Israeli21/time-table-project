9a4a4bcc8c1a37ba9f42a5b79a96506abf460cbe
Israel Trejo <isrtrejo07@gmail.com>
2025-01-19 21:48:20 -0600

    Able to store events into localStorage

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,4 +1,7 @@
-const todoList = [{
+// Load todoList from localStorage if it exists
+// To RESET todoList items to original items. In the console you type:
+// localStorage.removeItem('todoList')
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [{
     name: 'make dinner',
     dueDate: '2022-12-22'
 }, {
@@ -6,46 +9,50 @@ const todoList = [{
     dueDate: '2022-12-22'
 }];
 
+// Render the todo list on page load
 renderTodoList();
 
+function saveToStorage() {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
 // Regular Functions enable Hoisting
-function renderTodoList(){
+function renderTodoList() {
     let todoListHTML = '';
-    todoList.forEach((todoObject, index) =>{
-        const {name, dueDate} = todoObject;
+    todoList.forEach((todoObject, index) => {
+        const { name, dueDate } = todoObject;
         const html = `
         <div>${name}</div>
         <div>${dueDate}</div>
-            <button class = "delete-todo-button js-delete-todo-button">Delete</button>
-        `;  // Generating an HTML
+            <button class="delete-todo-button js-delete-todo-button">Delete</button>
+        `;
         todoListHTML += html;
     });
 
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
-    // querySeletorALL: will give us ALL the elments with '.js-delete-todo-button'
     document.querySelectorAll('.js-delete-todo-button')
         .forEach((deleteButton, index) => {
             deleteButton.addEventListener('click', () => {
                 todoList.splice(index, 1);
                 renderTodoList();
             });
-    });
+        });
+
+    saveToStorage();
 }
 
 document.querySelector('.js-add-todo-button').addEventListener('click', () => {
     addTodo();
 });
 
-function addTodo(){
+function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
-    
+
     const dateInputElement = document.querySelector('.js-due-date-input');
     const dueDate = dateInputElement.value;
 
     todoList.push({
-        // name: name,
-        // dueDate: dueDate,
         name,
         dueDate
     });
@@ -54,6 +61,7 @@ function addTodo(){
 
     renderTodoList();
 }
+
 
 // Closure:
 // - If a function has access to a value
